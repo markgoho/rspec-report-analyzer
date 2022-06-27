fe17d93e29a62d430d91528af4b9327411a67572
Mark Goho <markgoho@gmail.com>
2022-06-27 13:10:36 +0000

    update jest to v28

--- test/rspec-report-runtime.spec.ts
+++ test/rspec-report-runtime.spec.ts
@@ -1,8 +1,9 @@
+import { expect, test, describe } from '@jest/globals';
 import { rspecExamplesShort } from './mocks';
 import { rspecReportRuntime } from '../src';
 
 describe('Rspec Report Total Runtime', () => {
-  it('should produce the total runtime of a set of rspec examples', () => {
+  test('should produce the total runtime of a set of rspec examples', () => {
     expect(rspecReportRuntime(rspecExamplesShort)).toBeCloseTo(1.904_241_177, 5);
   });
 });
